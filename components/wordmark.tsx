73caddcb61bc16b9be6ae808eff340c23a99aa93
Rukayat Amzat <ayomideamzat8@gmail.com>
2026-08-15 11:51:59 +0100

    favicon

--- components/wordmark.tsx
+++ components/wordmark.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import aethelloMark from "@/app/icon.png";
 
 export function Wordmark() {
   return (
     <Link className="wordmark focus-ring" href="/" aria-label="Aethello home">
-      <span aria-hidden="true">A</span>
+      <Image alt="" aria-hidden="true" height={32} priority src={aethelloMark} width={32} />
       <strong>Aethello</strong>
     </Link>
   );
